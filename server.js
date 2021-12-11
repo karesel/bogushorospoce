0dc988734462a2cca2385bfc52320c27739e4bef
karesel <doguservan@hotmail.com>
2021-12-11 17:41:48 +0300

    add images to index, make index responsive, add styles

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const pageinfogen = require("./pageinfogen")
 
 const app = express()
 app.set("view engine", "ejs")
+app.use(express.static("public"))
 
 app.get("/", (req, res) => { res.render("index") })
 
